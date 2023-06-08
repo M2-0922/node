2ad13c58b2c930398350fe77df40ab8881954fed
Mayu <mayu.i.19930327@gmail.com>
2023-06-07 17:31:27 -0700

    added updata and delete contorollers

--- week4/day2/psql/controller/user.js
+++ week4/day2/psql/controller/user.js
@@ -45,8 +45,8 @@ const getUserById = (req, res) => {
     const { id } = req.params;
 
     try {
-        pool.query("SELECT * FROM users WHERE id = $1", [id], (err, result) => {
-            if(err) {
+        pool.query("SELECT * FROM users WHERE id = $1", [id], (error, result) => {
+            if (error) {
                 throw err;
             }
 
@@ -63,8 +63,49 @@ const getUserById = (req, res) => {
 
 // TODO: write update and delete controllers
 
+const updataUserById = (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    try {
+        pool.query(
+            "UPDATA users SET id = $1, name = $2, email = $3",
+            [updataUserById.name, updataUserById.email],
+            (error, result) => {
+            if (error) {
+                throw err;
+            }
+            res.json({
+                user: result.rows
+            })
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "server error"
+        })
+    }
+}
+
+const deleteUserById = (req, res) => {
+    const { id } = req.params;
+
+    try {
+        pool.query("DELETE FROM users WHERE id = $1", [deleteUserById], (error, result) => {
+            if (error) {
+                throw error;
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "server error"
+        })
+    }
+}
+
 export {
     createUser,
     getUsers,
-    getUserById
+    getUserById,
+    updataUserById,
+    deleteUserById
 }
--- week4/day2/psql/route/user.js
+++ week4/day2/psql/route/user.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createUser, getUsers, getUserById } from "../controller/user.js"
+import { createUser, getUsers, getUserById, updataUserById, deleteUserById } from "../controller/user.js"
 
 const router = Router();
 
 router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserById);
+router.put("/:id", updataUserById);
+router.delete("/:id", deleteUserById);
 
 // TODO: write update and delete routes
 
