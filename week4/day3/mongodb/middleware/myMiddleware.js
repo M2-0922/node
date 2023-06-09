f1abd9024d465757d5f75329a1ebab7435c1ead5
Mateus <mateuscgr@gmail.com>
2023-06-09 12:15:20 -0700

    lab done

---
+++ week4/day3/mongodb/middleware/myMiddleware.js
@@ -0,0 +1,16 @@
+import Post from "../model/post";
+
+const whoOwner = async (req, res, next) =>{
+    const { id } = req.params;
+    const userId = req.user.id
+    const post = await Post.findById(id);
+
+    if(post.author === userId) {
+        next();
+    }
+    else {
+        res.status(401).json({
+            message: "Unauthorized access"
+          });
+    }
+}
