1d9d3d84bc2a8675e6655f304460d8a4d6e3bde9
Kubilay <kubilayckmk@gmail.com>
2023-06-09 12:20:32 -0700

    check owner of post middleware

--- week4/day3/mongodb/route/post.js
+++ week4/day3/mongodb/route/post.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 
 import authentication from "../middleware/authentication.js"
 import { getAllPosts, getPostById, createAPost, updatePost, deletePost } from "../controller/post.js"
+import checkOwner from "../middleware/checkOwner.js";
 
 const router = Router();
 
 router.get("/", getAllPosts);
 router.get("/:id", getPostById);
 router.post("/", authentication, createAPost);
-router.put("/:id", authentication, updatePost);
-router.delete("/:id", authentication, deletePost)
+router.put("/:id", authentication, checkOwner, updatePost);
+router.delete("/:id", authentication,checkOwner, deletePost)
 
 export default router;
