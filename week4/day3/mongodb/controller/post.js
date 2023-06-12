302a671b5c93598f60636d5f67d92b64c53481eb
Yuya Minamide <dettu1003@gmail.com>
2023-06-12 09:48:46 -0700

    created comment and like controller

--- week4/day3/mongodb/controller/post.js
+++ week4/day3/mongodb/controller/post.js
@@ -1,99 +1,218 @@
-import Post from "../model/post.js"
+import Post from "../model/post.js";
 
 const getAllPosts = async (req, res, next) => {
-    
-    try {
-        let posts = await Post.find({});
-        
-        res.status(200).json({
-            posts
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: "Server error"
-        })
-    }
-}
+	try {
+		let posts = await Post.find({});
+
+		res.status(200).json({
+			posts,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Server error",
+		});
+	}
+};
+
 const getPostById = async (req, res, next) => {
-    const { id } = req.params;
+	const { id } = req.params;
 
-    try {
-        const post = await Post.findById(id);
+	try {
+		const post = await Post.findById(id);
 
-        if(!post){
-            return res.status(404).json({
-                message: `ID: ${id} of post couldn't found.`
-            })
-        }
+		if (!post) {
+			return res.status(404).json({
+				message: `ID: ${id} of post couldn't found.`,
+			});
+		}
 
-        return res.status(200).json({
-            post
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: "Server error"
-        })
-    }
-}
+		return res.status(200).json({
+			post,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			message: "Server error",
+		});
+	}
+};
+
 const createAPost = async (req, res, next) => {
-    try {
-        const postData = req.body;
+	try {
+		const postData = req.body;
 
-        let post = new Post(postData);
+		let post = new Post(postData);
 
-        await post.save();
+		await post.save();
 
-        
-
-        res.status(201).json({
-            post
-        })
-    } catch (error) {
-        
-    }
-}
+		res.status(201).json({
+			post,
+		});
+	} catch (error) {}
+};
 const updatePost = async (req, res, next) => {
-    const { id } = req.params;
-    const { title, article, tags, image } = req.body;
+	const { id } = req.params;
+	const { title, article, tags, image } = req.body;
 
-    try {
-        const post = await Post.findByIdAndUpdate(id, {
-            title,
-            article,
-            tags,
-            image
-        }, { new: true });
+	try {
+		const post = await Post.findByIdAndUpdate(
+			id,
+			{
+				title,
+				article,
+				tags,
+				image,
+			},
+			{ new: true }
+		);
 
-        res.status(200).json({
-            post
-        })
+		res.status(200).json({
+			post,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Server error",
+		});
+	}
+};
 
-    } catch (error) {
-        res.status(500).json({
-            message: "Server error"
-        })
-    }
-}
 const deletePost = async (req, res, next) => {
-    const { id } = req.params;
+	const { id } = req.params;
 
-    try {
-        await Post.findByIdAndDelete(id);
+	try {
+		await Post.findByIdAndDelete(id);
 
-        res.status(200).json({
-            message: `Post: ${id} is deleted!`
-        })
-    } catch (error) {
-        res.status(500).json({
-            message: `Server error`
-        })
-    }
-}
+		res.status(200).json({
+			message: `Post: ${id} is deleted!`,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: `Server error`,
+		});
+	}
+};
 
-export {
-    getAllPosts,
-    getPostById,
-    createAPost,
-    updatePost,
-    deletePost
-}
+const createComment = async (req, res, next) => {
+	const { id } = req.params;
+	const { comment } = req.body;
+
+	try {
+		const post = await Post.findById(id);
+
+		if (!post) {
+			return res.status(404).json({
+				message: `Post with ID: ${id} not found.`,
+			});
+		}
+
+		post.comments.push(comment);
+		await post.save();
+
+		res.status(201).json({
+			message: "Comment added successfully",
+			post,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Server error",
+		});
+	}
+};
+
+const deleteComment = async (req, res, next) => {
+	const { id } = req.params;
+	const { commentId } = req.body;
+
+	try {
+		const post = await Post.findById(id);
+
+		if (!post) {
+			return res.status(404).json({
+				message: `Post with ID: ${id} not found.`,
+			});
+		}
+
+		post.comments = post.comments.filter((c) => c._id.toString() !== commentId);
+		await post.save();
+
+		res.status(200).json({
+			message: "Comment deleted successfully",
+			post,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Server error",
+		});
+	}
+};
+
+const likePost = async (req, res, next) => {
+	const { id } = req.params;
+	const { userId } = req.body;
+
+	try {
+		const post = await Post.findById(id);
+
+		if (!post) {
+			return res.status(404).json({
+				message: `Post with ID: ${id} not found.`,
+			});
+		}
+
+		const likedByUser = post.likes.includes(userId);
+
+		if (likedByUser) {
+			return res.status(400).json({
+				message: "Post already liked by the user.",
+			});
+		}
+
+		post.likes.push(userId);
+		await post.save();
+
+		res.status(201).json({
+			message: "Post liked successfully",
+			post,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Server error",
+		});
+	}
+};
+
+const unlikePost = async (req, res, next) => {
+	const { id } = req.params;
+	const { userId } = req.body;
+
+	try {
+		const post = await Post.findById(id);
+
+		if (!post) {
+			return res.status(404).json({
+				message: `Post with ID: ${id} not found.`,
+			});
+		}
+
+		const likedByUser = post.likes.includes(userId);
+
+		if (!likedByUser) {
+			return res.status(400).json({
+				message: "Post not liked by the user.",
+			});
+		}
+
+		post.likes = post.likes.filter((like) => like.toString() !== userId);
+		await post.save();
+
+		res.status(200).json({
+			message: "Post unliked successfully",
+			post,
+		});
+	} catch (error) {
+		res.status(500).json({
+			message: "Server error",
+		});
+	}
+};
+
+export { getAllPosts, getPostById, createAPost, updatePost, deletePost, createComment, deleteComment, likePost, unlikePost };
--- week4/day3/mongodb/route/post.js
+++ week4/day3/mongodb/route/post.js
@@ -1,7 +1,17 @@
 import { Router } from "express";
 
-import authentication from "../middleware/authentication.js"
-import { getAllPosts, getPostById, createAPost, updatePost, deletePost } from "../controller/post.js"
+import authentication from "../middleware/authentication.js";
+import {
+	getAllPosts,
+	getPostById,
+	createAPost,
+	updatePost,
+	deletePost,
+	createComment,
+	deleteComment,
+	likePost,
+	unlikePost,
+} from "../controller/post.js";
 import checkOwner from "../middleware/checkOwner.js";
 
 const router = Router();
@@ -10,6 +20,10 @@ router.get("/", getAllPosts);
 router.get("/:id", getPostById);
 router.post("/", authentication, createAPost);
 router.put("/:id", authentication, checkOwner, updatePost);
-router.delete("/:id", authentication,checkOwner, deletePost)
+router.delete("/:id", authentication, checkOwner, deletePost);
+router.post("/:id/comments", authentication, createComment);
+router.delete("/:id/comments", authentication, deleteComment);
+router.post("/:id/like", authentication, likePost);
+router.delete("/:id/like", authentication, unlikePost);
 
 export default router;
